a9d3019cc731287d48b79f89cd8b20cf68c10245
muniprasad03 <muniprasad.ch@gmail.com>
2025-11-20 12:33:27 +0530

    Updates

---
+++ assets/oops-DcwBcRO8.js
@@ -0,0 +1,1 @@
+const e={slug:"oops",name:"OOPS",questions:[{q:"What is C#?",a:"C# is a modern, object-oriented programming language developed by Microsoft. It is part of the .NET framework and is used for developing Windows applications, web applications, and web services."}]};export{e as default};
