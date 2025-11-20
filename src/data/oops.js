4d1734a00d8f1dadc29c1b12a7cb4f42b5eb3b22
muniprasad03 <muniprasad.ch@gmail.com>
2025-11-20 12:32:26 +0530

    5

---
+++ src/data/oops.js
@@ -0,0 +1,12 @@
+// src/data/csharp.js
+export default {
+  slug: "oops",
+  name: "OOPS",
+  questions: [
+    {
+      q: "What is C#?",
+      a: "C# is a modern, object-oriented programming language developed by Microsoft. It is part of the .NET framework and is used for developing Windows applications, web applications, and web services.",
+    },
+    
+  ],
+};
